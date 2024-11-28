180f6d9222b71f9398ef1a13583e2f981a998dc7
laerkehansen <laha0007@stud.kea.dk>
2024-11-28 09:00:46 +0100

    det virker sgu

---
+++ src/app/components/newsletter.jsx
@@ -0,0 +1,56 @@
+import { getSubs, postSubs, patchSub } from "@/app/lib/supabase";
+import { revalidatePath } from "next/cache";
+import Link from "next/link";
+
+async function Newsletter() {
+  const subscribers = await getSubs();
+
+  console.log(subscribers);
+  async function send(FormData) {
+    "use server";
+    const data = {
+      navn: FormData.get("navn"),
+      email: FormData.get("email"),
+    };
+
+    console.log(data);
+    await postSubs(data);
+
+    //det her er hvor min route er, altså i dette tilfælde apitest
+    revalidatePath("/");
+  }
+  return (
+    <form action={send} className="bg-white w-fit rounded-lg p-3 grid gap-2">
+      <legend>
+        <h1 className="text-black text-2xl">Newsletter</h1>
+      </legend>
+      <div className="flex flex-col">
+        <label htmlFor="name" className="text-black">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="navn"
+          className="bg-gray-100 text-gray-700"
+          required="true"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="email" className="text-black">
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          required="true"
+          className="bg-gray-100 text-gray-700"
+        />
+      </div>
+      <button className="bg-blue-500 rounded-xl p-3">Subscribe</button>
+    </form>
+  );
+}
+
+export default Newsletter;
---
+++ src/app/lib/supabase.js
@@ -0,0 +1,62 @@
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const headersList = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+  Prefer: "return=representation",
+  apikey: key,
+};
+
+export async function getSubs() {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: headersList,
+  });
+
+  const data = await response.json();
+  //   console.log(data);
+  return data;
+}
+
+export async function getSubsById(id) {
+  const response = await fetch(`${url}?id=eq.${id}`, {
+    method: "GET",
+    headers: headersList,
+  });
+
+  const data = await response.json();
+  //   console.log(data);
+  return data;
+}
+
+export async function postSubs(subdata) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: headersList,
+    body: JSON.stringify(subdata),
+  });
+
+  const data = await response.json();
+  //   console.log(data);
+  return data;
+}
+
+export async function patchSub(id, patchData) {
+  const response = await fetch(`${url}?id=eq.${id}`, {
+    method: "PATCH",
+    headers: headersList,
+    body: JSON.stringify(patchData),
+  });
+  const data = await response.json();
+  return data;
+}
+
+export async function deleteSub(id) {
+  const response = await fetch(`${url}?id=eq.${id}`, {
+    method: "DELETE",
+    headers: headersList,
+  });
+  const data = await response.json();
+  return data;
+}
