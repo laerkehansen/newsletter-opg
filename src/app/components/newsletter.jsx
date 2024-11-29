f99bf393e719f36cc1b41aa4e47c9ec5a21f2fec
laerkehansen <laha0007@stud.kea.dk>
2024-11-29 09:49:36 +0100

    send

--- src/app/components/newsletter.jsx
+++ src/app/components/newsletter.jsx
@@ -1,25 +1,24 @@
-import { getSubs, postSubs } from "@/app/lib/supabase";
+import { postSubs, patchSub } from "@/app/lib/supabase";
 import { revalidatePath } from "next/cache";
+import Link from "next/link";
 
 async function Newsletter() {
-  // const subscribers = await getSubs();
+  console.log(subscribers);
+  async function send(FormData) {
+    "use server";
+    const data = {
+      navn: FormData.get("navn"),
+      email: FormData.get("email"),
+    };
 
-  // console.log(subscribers);
-  // async function send(FormData) {
-  //   "use server";
-  //   const data = {
-  //     navn: FormData.get("navn"),
-  //     email: FormData.get("email"),
-  //   };
+    console.log(data);
+    await postSubs(data);
 
-  //   console.log(data);
-  //   await postSubs(data);
-
-  //   //det her er hvor min route er, altså i dette tilfælde apitest
-  //   revalidatePath("/");
-  // }
+    //det her er hvor min route er, altså i dette tilfælde apitest
+    revalidatePath("/");
+  }
   return (
-    <form className="bg-white w-fit rounded-lg p-3 grid gap-2">
+    <form action={send} className="bg-white w-fit rounded-lg p-3 grid gap-2">
       <legend>
         <h1 className="text-black text-2xl">Newsletter</h1>
       </legend>
