6da88d8a7be0aa628c746e870afef27a83c200f4
laerkehansen <laha0007@stud.kea.dk>
2024-11-29 09:24:54 +0100

    ..

--- src/app/apitest/page.jsx
+++ src/app/apitest/page.jsx
@@ -1,4 +1,4 @@
-// import Link from "next/link";
+import Link from "next/link";
 import Newsletter from "@/app/components/newsletter";
 
 import { getSubs } from "@/app/lib/supabase";
@@ -19,12 +19,12 @@ export default async function Home() {
         <ul className="flex flex-wrap gap-3 max-w-sm">
           {subscribers.map((sub) => (
             <li key={sub.id} className="min-w-24 m">
-              {/* <Link href={`./apitest/update/${sub.id}`}> */}
-              <div className="bg-white p-2 rounded text-black">
-                <p className="font-semibold">{sub.navn}</p>
-                <p className="text-gray-600">{sub.email}</p>
-              </div>
-              {/* </Link> */}
+              <Link href={`./update/${sub.id}`}>
+                <div className="bg-white p-2 rounded text-black">
+                  <p className="font-semibold">{sub.navn}</p>
+                  <p className="text-gray-600">{sub.email}</p>
+                </div>
+              </Link>
             </li>
           ))}
         </ul>
